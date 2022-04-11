ecc5141ea88dad6c5041e18d8415588579c87f70
Gabriel Kępka <gkepka@student.agh.edu.pl>
2022-04-11 04:55:00 +0200

    Added endpoint tests

--- src/test/java/com/virtuslab/internship/MyResourceTest.java
+++ src/test/java/com/virtuslab/internship/MyResourceTest.java
@@ -25,7 +25,7 @@ class MyResourceTest extends JerseyTest {
     }
 
     @Test
-    void getReceipt() {
+    void getReceipt10Discount() {
         //Given
         var productDb = new ProductDb();
         var cart = new Basket();
@@ -52,5 +52,64 @@ class MyResourceTest extends JerseyTest {
         assertEquals(5, receipt.entries().size());
         assertEquals(expectedTotalPrice, receipt.totalPrice());
         assertEquals(1, receipt.discounts().size());
+    }
+
+    @Test
+    void getReceipt15Discount() {
+        //Given
+        var productDb = new ProductDb();
+        var cart = new Basket();
+        var bread = productDb.getProduct("Bread");
+        var banana = productDb.getProduct("Banana");
+        var cereals = productDb.getProduct("Cereals");
+
+        var expectedTotalPrice = bread.price().add(banana.price()).add(cereals.getPrice().multiply(BigDecimal.valueOf(2))).multiply(BigDecimal.valueOf(0.85));
+
+        cart.addProduct(bread);
+        cart.addProduct(banana);
+        cart.addProduct(cereals);
+        cart.addProduct(cereals);
+
+        //When
+        var response = target("/basket").request().post(Entity.entity(cart, MediaType.APPLICATION_JSON));
+        var receipt = (Receipt) response.readEntity(Receipt.class);
+
+        // Then
+        assertNotNull(receipt);
+        assertEquals(4, receipt.entries().size());
+        assertEquals(expectedTotalPrice, receipt.totalPrice());
+        assertEquals(1, receipt.discounts().size());
+    }
+
+    @Test
+    void getReceiptBothDiscounts() {
+        //Given
+        var productDb = new ProductDb();
+        var cart = new Basket();
+        var bread = productDb.getProduct("Bread");
+        var banana = productDb.getProduct("Banana");
+        var cereals = productDb.getProduct("Cereals");
+        var steak = productDb.getProduct("Steak");
+        var cheese = productDb.getProduct("Cheese");
+
+        var expectedTotalPrice = bread.price().add(banana.price()).add(cereals.getPrice().multiply(BigDecimal.valueOf(2)))
+                .add(steak.getPrice()).add(cheese.getPrice()).multiply(BigDecimal.valueOf(0.85)).multiply(BigDecimal.valueOf(0.9));
+
+        cart.addProduct(bread);
+        cart.addProduct(banana);
+        cart.addProduct(cereals);
+        cart.addProduct(cereals);
+        cart.addProduct(steak);
+        cart.addProduct(cheese);
+
+        //When
+        var response = target("/basket").request().post(Entity.entity(cart, MediaType.APPLICATION_JSON));
+        var receipt = (Receipt) response.readEntity(Receipt.class);
+
+        // Then
+        assertNotNull(receipt);
+        assertEquals(6, receipt.entries().size());
+        assertEquals(expectedTotalPrice, receipt.totalPrice());
+        assertEquals(2, receipt.discounts().size());
     }
 }
